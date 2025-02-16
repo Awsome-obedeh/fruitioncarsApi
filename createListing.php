97d666e604970f0a1349cc7f16b3439db0fe416b
obed chidera <obedchidera1010@gmail.com>
2025-02-17 00:54:41 +0100

    update on create car and listing endpoint

--- createListing.php
+++ createListing.php
@@ -8,6 +8,7 @@ $data = json_decode($edata, true);
 $title = $data['title'];
 $description = $data['description'];
 $image_url=$data["image_url"];
+$car_gallery=$data["car_gallery"];
 $price=$data["price"];
 $category=$data["category"];
 $model=$data['model'];
@@ -18,17 +19,20 @@ $color=$data["color"];
 
 
 if($_SERVER['REQUEST_METHOD']=="POST"){
+    if (!empty($car_gallery)) {
+        $value = json_encode($car_gallery);
+    }
 // insert admin details
 // remember to hash the user password 
-$insert_sql = "INSERT INTO carlisting (title, description, image_url, price, category, brand_name, model,  fuel_type, color) 
-VALUES ('$title', '$description', '$image_url', $price, '$category', '$brand_name', '$model',  '$fuel_type', '$color')";
+$insert_sql = "INSERT INTO carlisting (title, description, image_url, car_gallery,price, category, brand_name, model,  fuel_type, color) 
+VALUES ('$title', '$description', '$image_url', '$value', $price, '$category', '$brand_name', '$model',  '$fuel_type', '$color')";
 
 $insert_query=mysqli_query($conn,$insert_sql);
 
 if($insert_query){
     echo json_encode([
         "status"=>200,
-        "msg"=>"car addded successfully"
+        "msg"=>"car addded successfully to featured cars"
     ]);
 }
 else{
